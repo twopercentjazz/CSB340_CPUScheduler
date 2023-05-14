927ccff0c5586106fca5d5624a9d53a21c072632
twopercentjazz <chrissnickell@gmail.com>
2023-05-14 14:15:31 -0700

    implement AlgorithmsInterface

--- src/main/java/AdditionalUtilities/Algorithms/AlgorithmsInterface.java
+++ src/main/java/AdditionalUtilities/Algorithms/AlgorithmsInterface.java
@@ -1,4 +1,22 @@
+/** This interface represents all algorithm types. */
+
 package AdditionalUtilities.Algorithms;
+import AdditionalUtilities.Utilities.*;
+import java.util.Queue;
 
 public interface AlgorithmsInterface {
+
+    AlgorithmTypes getAlgorithmType();
+
+    void scheduleNextProcess();
+
+    void dispatchNextProcess(ProcessControlBlock running);
+
+    Boolean isCompleted();
+
+    Scheduler getScheduler();
+
+    Dispatcher getDispatcher();
+
+    Queue<ProcessControlBlock> getReady();
 }
